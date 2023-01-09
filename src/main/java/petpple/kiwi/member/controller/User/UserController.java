a42e7e6d4192420d3b5d5ed5c5ec276da2456abd
hyunkkong <gustjd8979@gmail.com>
2023-01-09 12:28:40 +0900

    ⚡️ :: user Controller 버전 이전 매핑 - 현성
    ⚡️ :: user Controller 버전 이전 매핑 - 현성

--- src/main/java/petpple/kiwi/member/controller/User/UserController.java
+++ src/main/java/petpple/kiwi/member/controller/User/UserController.java
@@ -23,4 +23,60 @@ public class UserController {
 	{
 		return "user/sample";
 	}
+	//------------------------FAQ 시작------------------------//
+	@RequestMapping("/user/FAQ")
+	public String FAQ()
+	{
+		return "user/FAQ";
+	}
+	//------------------------FAQ 종료------------------------//
+	
+	//------------------------의뢰자님을 위한 FAQ 시작------------------------//
+	@RequestMapping("/user/userFAQ")
+	public String userFAQ()
+	{
+		return "user/userFAQ";
+	}
+	//------------------------의뢰자님을 위한 FAQ 종료------------------------//
+	
+	//------------------------펫시터님을 위한 FAQ 시작------------------------//
+	@RequestMapping("/user/petsitterFAQ")
+	public String petsitterFAQ()
+	{
+		return "user/petsitterFAQ";
+	}
+	//------------------------펫시터님을 위한 FAQ 종료------------------------//
+	
+	//------------------------유저 메인페이지 시작------------------------//
+	@RequestMapping("/user/userMain")
+	public String Main()
+	{
+		return "user/userMain";
+	}
+	//------------------------유저 메인페이지 종료------------------------//
+	
+	//------------------------유저 회원가입 이용약관 시작------------------------//
+	@RequestMapping("/user/userContract")
+	public String userContract()
+	{
+		return "user/userContract";
+	}
+	//------------------------유저 회원가입 이용약관 종료------------------------//
+	
+	//------------------------유저 로그인 시작------------------------//
+	@RequestMapping("/user/userLogin")
+	public String userLogin()
+	{
+		return "user/userLogin";
+	}
+	//------------------------유저 로그인 종료------------------------//
+	
+	//------------------------유저 회원가입 시작------------------------//
+	@RequestMapping("/user/userSignUp")
+	public String userSignUp()
+	{
+		return "user/userSignUp";
+	}
+	//------------------------유저 회원가입 종료------------------------//
+	
 }
