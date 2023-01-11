e0f109e9ac31001cb6b09e08370afec0379c6c56
SOYEON <tivm8012@naver.com>
2023-01-11 11:06:31 +0900

    ⚡️ ::   member Controller value, method 수정 - 소연
    ⚡️ ::   member Controller value, method 수정 - 소연

--- src/main/java/petpple/kiwi/member/controller/Member/MemberController.java
+++ src/main/java/petpple/kiwi/member/controller/Member/MemberController.java
@@ -35,38 +35,45 @@ public class MemberController {
 	{
 		return "member/sample2";
 	}
+	// 로그인 페이지
+	@RequestMapping(value = "/member/memberLogin")
+	public String memberLogin()
+	{
+		return "member/memberLogin";
+	}
+	
 	
 
 	//-------------- 펫시터 지원하기 ------------------
-	@RequestMapping("/member/memberApply")
+	@RequestMapping(value = "/member/memberApply")
 	public String memberApply()
 	{
 		return "member/memberApply";
 	}
 	
-	@RequestMapping("/member/memberSubmit")
-	public String memberSubmit()
+	@RequestMapping(value = "/member/memberSubmit", method = RequestMethod.GET)
+	public String memberSubmit(Model model)
 	{
 		return "member/memberSubmit";
 	}
 	
 	//-------------- 펫시터 폼 관련 -----------------------------
 	// 일반회원 → 펫시터 지원 폼
-	@RequestMapping("/member/memberApplyForm")
+	@RequestMapping(value = "/member/memberApplyForm", method = RequestMethod.POST)
 	public String memberApplyForm()
 	{
 		return "member/memberApplyForm";
 	}
 	
 	// 의뢰인 긴급 요청 서비스 등록 폼
-	@RequestMapping("/member/memberUsvcForm")
+	@RequestMapping(value = "/member/memberUsvcForm")
 	public String memberUsvcForm()
 	{
 		return "member/memberUsvcForm";
 	}
 	
 	// 의뢰인 시터 후기 등록 폼
-	@RequestMapping("/member/memberReviewForm")
+	@RequestMapping(value = "/member/memberReviewForm")
 	public String memberReviewForm()
 	{
 		return "member/memberReviewForm";
@@ -74,19 +81,19 @@ public class MemberController {
 	
 	//-------------- 의뢰인 메인페이지  ------------------
 	// 중간 메인 화면
-	@RequestMapping("/member/memberMain")
+	@RequestMapping(value = "/member/memberMain")
 	public String memberMain()
 	{
 		return "member/memberMain";
 	}
 	// 오른쪽 달력(일정) 화면 사이드
-	@RequestMapping("/member/memberCal")
+	@RequestMapping(value = "/member/memberCal")
 	public String memberCal()
 	{
 		return "member/memberCal";
 	}
 	// 왼쪽 펫시팅 정보 사이드
-	@RequestMapping("/member/memberSide")
+	@RequestMapping(value = "/member/memberSide")
 	public String memberSide()
 	{
 		return "member/memberSide";
@@ -94,20 +101,20 @@ public class MemberController {
 	
 	// ----------- 의뢰인 마이페이지 ------------------
 	//  - 의뢰인 내 정보 변경
-	@RequestMapping("/member/memberMyInfo")
+	@RequestMapping(value = "/member/memberMyInfo")
 	public String memberMyInfo()
 	{
 		return "member/memberMyInfo";
 	}
 	
 	// -  의뢰인 내 펫 관리 
-	@RequestMapping("/member/memberMyPet")
+	@RequestMapping(value = "/member/memberMyPet")
 	public String membermemberMyPet()
 	{
 		return "member/memberMyPet";
 	}
 	
-	@RequestMapping("/member/memberMyBlock")
+	@RequestMapping(value = "/member/memberMyBlock")
 	public String memberMyBlock()
 	{
 		return "member/memberMyBlock";
@@ -117,7 +124,7 @@ public class MemberController {
 	
 	
 	//-------------- 의뢰인 긴급서비스 등록 디테일------------------
-	@RequestMapping("/member/memberUsvcDetail")
+	@RequestMapping(value = "/member/memberUsvcDetail")
 	public String memberUsvcDetail()
 	{
 		return "member/memberUsvcDetail";
@@ -125,20 +132,20 @@ public class MemberController {
 	
 	
 	// 의뢰인 긴급서비스/ 방문, 위탁 서비스 타입 선택 페이지
-	@RequestMapping("/member/memberSvcTypeCho")
+	@RequestMapping(value = "/member/memberSvcTypeCho")
 	public String memberSvcTypeCho()
 	{
 		return "member/memberSvcTypeCho";
 	}
 	// 의뢰인 긴급서비스/ 데이, 타임 케어 타입 선택 페이지
-	@RequestMapping("/member/memberCareTypeCho")
+	@RequestMapping(value = "/member/memberCareTypeCho")
 	public String membeCareTypeCho()
 	{
 		return "member/memberCareTypeCho";
 	}
 	
 	/* 의뢰인 긴급서비스 요청 할 펫 선택 페이지 */
-	@RequestMapping("/member/memberUsvcPetCho")
+	@RequestMapping(value = "/member/memberUsvcPetCho")
 	public String memberUsvcPetCho()
 	{
 		return "member/memberUsvcPetCho";
@@ -147,35 +154,35 @@ public class MemberController {
 	
 	
 	/* 의뢰인 내 펫 등록(추가) 페이지 */
-	@RequestMapping("/member/memberMyPetInsert")
+	@RequestMapping(value = "/member/memberMyPetInsert")
 	public String memberMyPetInsert()
 	{
 		return "member/memberMyPetInsert";
 	}
 	
 	/* 의뢰인 후기 쓸 펫 선택 */
-	@RequestMapping("/member/memberReviewPetChoice")
+	@RequestMapping(value = "/member/memberReviewPetChoice")
 	public String memberReviewPetChoice()
 	{
 		return "member/memberReviewPetChoice";
 	}
 	
 	/* 의뢰인 memberFAQ */
-	@RequestMapping("/member/memberFAQ")
+	@RequestMapping(value = "/member/memberFAQ")
 	public String memberFAQ()
 	{
 		return "member/memberFAQ";
 	}
 	
 	/* 의뢰인 내 펫 수정 폼 */
-	@RequestMapping("/member/memberMyPetUpdate")
+	@RequestMapping(value = "/member/memberMyPetUpdate")
 	public String memberMyPetUpdate()
 	{
 		return "member/memberMyPetUpdate";
 	}
 
 	/* 의뢰인 작성된 리뷰  */
-	@RequestMapping("/member/memberReviewWritten")
+	@RequestMapping(value = "/member/memberReviewWritten")
 	public String memberReviewWritten()
 	{
 		return "member/memberReviewWritten";
@@ -184,19 +191,19 @@ public class MemberController {
 	/* --------------- 의뢰인 펫시팅 정보 왼쪽 Side---------------  */
 	
 	// 의뢰인 펫시팅 정보/ 긴급 요청 서비스 - 현재 의뢰중
-	@RequestMapping("/member/memberInfoUsvcIng")
+	@RequestMapping(value = "/member/memberInfoUsvcIng")
 	public String memberInfoUsvcIng()
 	{
 		return "member/memberInfoUsvcIng";
 	}
 	// 의뢰인 펫시팅 정보/ 긴급 요청 서비스 - 의뢰인 수락 대기중 페이지
-	@RequestMapping("/member/memberInfoUsvcWaiting")
+	@RequestMapping(value = "/member/memberInfoUsvcWaiting")
 	public String memberInfoUsvcWaiting()
 	{
 		return "member/memberInfoUsvcWaiting";
 	}
 	// 의뢰인 펫시팅 정보/ 긴급 요청 서비스 - 누적 펫시팅 이력 페이지
-	@RequestMapping("/member/memberInfoUsvcPast")
+	@RequestMapping(value = "/member/memberInfoUsvcPast")
 	public String memberInfoUsvcPast()
 	{
 		return "member/memberInfoUsvcPast";
@@ -206,13 +213,13 @@ public class MemberController {
 	
 
 	// 의뢰인 펫시팅 정보/ 방문서비스 - 펫시터 수락 대기중
-	@RequestMapping("/member/memberInfoVsvcWaiting")
+	@RequestMapping(value = "/member/memberInfoVsvcWaiting")
 	public String memberInfoVsvcWaiting()
 	{
 		return "member/memberInfoVsvcWaiting";
 	}
 	// 의뢰인 펫시팅 정보/ 방문서비스 - 누적 펫시팅 이력 페이지
-	@RequestMapping("/member/memberInfoVsvcPast")
+	@RequestMapping(value = "/member/memberInfoVsvcPast")
 	public String memberInfoVsvcPast()
 	{
 		return "member/memberInfoVsvcPast";
@@ -220,18 +227,33 @@ public class MemberController {
 
 	
 	// 의뢰인 펫시팅 정보/ 위탁서비스 - 펫시터 수락 대기중
-	@RequestMapping("/member/memberInfoFsvcWaiting")
+	@RequestMapping(value = "/member/memberInfoFsvcWaiting")
 	public String memberInfoFsvcWaiting()
 	{
 		return "member/memberInfoFsvcWaiting";
 	}
 	// 의뢰인 펫시팅 정보/ 위탁서비스 - 누적 펫시팅 이력 페이지
-	@RequestMapping("/member/memberInfoFsvcPast")
+	@RequestMapping(value = "/member/memberInfoFsvcPast")
 	public String memberInfoFsvcPast()
 	{
 		return "member/memberInfoFsvcPast";
 	}
 	
+	// 위탁 펫시터 리스트 페이지
+	@RequestMapping(value = "/member/fsitterPetSitterList")
+	public String fsitterPetSitterList()
+	{
+		return "member/fsitterPetSitterList";
+	}
+	
+	// 방문 펫시터 리스트 페이지
+	@RequestMapping(value = "/member/vsitterPetSitterList")
+	public String vsitterPetSitterList()
+	{
+		return "member/vsitterPetSitterList";
+	}
+			
+		
 
 	
 }
