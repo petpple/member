94adb5e03daf04b62576876c288ecf12e4e6fe0e
flown222 <jangu3384@gmail.com>
2023-01-11 10:48:46 +0900

    ⚡️ :: 펫시터 지원 컨트롤러 생성 -동현,영관
    ⚡️ :: 펫시터 지원 컨트롤러 생성 -동현 ,영관

---
+++ src/main/java/petpple/kiwi/member/controller/Member/MemberToSitterController.java
@@ -0,0 +1,35 @@
+package petpple.kiwi.member.controller.Member;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import petpple.kiwi.member.domain.apply.Sitter;
+import petpple.kiwi.member.repository.apply.ISitterApply;
+
+@Controller
+public class MemberToSitterController {
+
+	@Autowired
+	private SqlSession sqlSession;
+
+	// -------------- 펫시터 폼 관련 -----------------------------
+	// 일반회원 → 펫시터 지원 폼
+	@RequestMapping("/member/memberApplyForm")
+	public String memberApplyForm() {
+		return "member/memberApplyForm";
+	}
+
+	@RequestMapping(value = "/member/sitterApply", method = RequestMethod.POST)
+	public String sitterApply(Sitter dto) {
+
+		ISitterApply dao = sqlSession.getMapper(ISitterApply.class);
+		dao.applySitter(dto);
+
+		return "redirect:memberMain";
+	}
+
+
+}
