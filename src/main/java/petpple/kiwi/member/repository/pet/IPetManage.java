587c233c6d4a2273494975bc5cc2b465f6f92842
younggwan <johnbuuk1@gmail.com>
2023-01-11 14:02:26 +0900

    📑 :: 펫 추가 인터페이스 생성 - 영관
    📑 :: 펫 추가 인터페이스 생성 - 영관

---
+++ src/main/java/petpple/kiwi/member/repository/pet/IPetManage.java
@@ -0,0 +1,13 @@
+package petpple.kiwi.member.repository.pet;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import petpple.kiwi.member.domain.pet.Pet;
+
+@Mapper
+public interface IPetManage {
+
+	public int insertPet(Pet dto);
+	
+	
+}
