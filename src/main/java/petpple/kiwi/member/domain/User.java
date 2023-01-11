14ec80fc3f3a47b28ae1d9013adb6163035844a5
hyunkkong <gustjd8979@gmail.com>
2023-01-11 10:42:51 +0900

    ⚡️ :: user DTO 수정 - 현성
    ⚡️ :: user DTO 수정 - 현성

--- src/main/java/petpple/kiwi/member/domain/User.java
+++ src/main/java/petpple/kiwi/member/domain/User.java
@@ -4,16 +4,14 @@ import lombok.Data;
 
 @Data
 public class User {
-	// 회원번호
-	private int Id;
 	/*
 	이름, 아이디, 비밀번호
 	, 전화번호, 생년월일, 주민앞, 주민뒤, 닉네임
 	, 우편번호, 주소, 상세주소, 위도, 경도
 	, 이미지경로, 임시회원번호 
 	*/
-	private String mem_name, mem_id_id, mem_pw
-	, tel, date, ssn1, ssn2, nickname
+	private String name, id, pw
+	, tel, ssn1, ssn2, nickname
 	, code, addr, detailAddress, lat, lng
-	, profile, email, tmp_member_id;
+	, email;
 }
