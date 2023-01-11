b0307db07f53e40984a7c2eecba61a02dabe9685
flown222 <jangu3384@gmail.com>
2023-01-11 18:09:38 +0900

    ⚡️ :: 펫 인서트 컨트롤러 생성 -동현,영관
    ⚡️ :: 펫 인서트 컨트롤러 생성 -동현,영관

--- src/main/java/petpple/kiwi/member/controller/Member/MemberPetInsertController.java
+++ src/main/java/petpple/kiwi/member/controller/Member/MemberPetInsertController.java
@@ -3,11 +3,16 @@ package petpple.kiwi.member.controller.Member;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import petpple.kiwi.member.domain.pet.Pet;
 import petpple.kiwi.member.repository.pet.IPetManage;
+import petpple.kiwi.member.service.member.ImgUpload;
+import petpple.kiwi.member.service.member.MemberService;
 
 @Controller
 public class MemberPetInsertController {
@@ -24,11 +29,12 @@ public class MemberPetInsertController {
 	}
 	
 	@RequestMapping(value = "/member/petInsert" ,method = RequestMethod.POST)
-	public String membermemberMyPetInsert(Pet dto)
+	public String membermemberMyPetInsert(Pet dto,@RequestParam(value = "file")MultipartFile file)
 	{
 		IPetManage daoIPetManage = sqlSession.getMapper(IPetManage.class );
 		daoIPetManage.insertPet(dto);
 		
+		new ImgUpload().uploadProfileImg(file);
 		return "redirect:memberMain";
 	}
 	
