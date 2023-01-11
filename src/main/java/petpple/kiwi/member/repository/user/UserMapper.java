63d9400557e860baebb0c9f0bd0adf5439437072
SOYEON <tivm8012@naver.com>
2023-01-11 17:12:04 +0900

    ⚡️ ::  userMapper 접근제어지시자  수정 - 소연
    ⚡️ ::  userMapper 접근제어지시자  수정 - 소연

--- src/main/java/petpple/kiwi/member/repository/user/UserMapper.java
+++ src/main/java/petpple/kiwi/member/repository/user/UserMapper.java
@@ -3,27 +3,29 @@ package petpple.kiwi.member.repository.user;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import petpple.kiwi.member.domain.user.User;
 
 @Mapper
 public interface UserMapper {
 
-	int PRC_TMP_MEMBER(User user);
+	public int PRC_TMP_MEMBER(User user);
 	
-	int deleteUser(int id);
+	public int deleteUser(int id);
 	
-	int getCount();
+	public int getCount();
 	
-	List<User> getUserList();
+	//public List<User> getUserList();
 	
-	User userLogin(User user);
 	
+	
+	public int userLogin(User user);
+	
+	public String tmpMemberId(String id, String pw);
+
 		
 }
-
-
-
 
 
 
