e0e3954a4b6acba34db4a77d3d51811291489d73
flown222 <jangu3384@gmail.com>
2023-01-12 10:20:26 +0900

    ⚡️ :: 펫시터 지원 dto수정 -동현
    ⚡️ :: 펫시터 지원 dto수정 -동현

--- src/main/java/petpple/kiwi/member/domain/apply/Sitter.java
+++ src/main/java/petpple/kiwi/member/domain/apply/Sitter.java
@@ -1,79 +1,11 @@
 package petpple.kiwi.member.domain.apply;
 
+import lombok.Data;
+
+@Data
 public class Sitter {
-	String title, content, depositor
+	String id,title, content, depositor
 	, accNumber
 	, smokeId, petSizeId
 	, petKindId, bankId;
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
-
-	public String getDepositor() {
-		return depositor;
-	}
-
-	public void setDepositor(String depositor) {
-		this.depositor = depositor;
-	}
-
-	public String getAccNumber() {
-		return accNumber;
-	}
-
-	public void setAccNumber(String accNumber) {
-		this.accNumber = accNumber;
-	}
-
-	/*
-	 * public String getTmpMemberId() { return tmpMemberId; }
-	 * 
-	 * public void setTmpMemberId(String tmpMemberId) { this.tmpMemberId =
-	 * tmpMemberId; }
-	 */
-
-	public String getSmokeId() {
-		return smokeId;
-	}
-
-	public void setSmokeId(String smokeId) {
-		this.smokeId = smokeId;
-	}
-
-	public String getPetSizeId() {
-		return petSizeId;
-	}
-
-	public void setPetSizeId(String petSizeId) {
-		this.petSizeId = petSizeId;
-	}
-
-	public String getPetKindId() {
-		return petKindId;
-	}
-
-	public void setPetKindId(String petKindId) {
-		this.petKindId = petKindId;
-	}
-
-	public String getBankId() {
-		return bankId;
-	}
-
-	public void setBankId(String bankId) {
-		this.bankId = bankId;
-	}
 }
