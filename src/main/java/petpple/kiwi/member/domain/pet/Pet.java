07af287943a2c7d2c7b179daf5873b3f0bee9cac
flown222 <jangu3384@gmail.com>
2023-01-16 00:25:06 +0900

    ⚡️ ::  펫 dto 수정-동현
    ⚡️ ::  펫 dto 수정-동현

--- src/main/java/petpple/kiwi/member/domain/pet/Pet.java
+++ src/main/java/petpple/kiwi/member/domain/pet/Pet.java
@@ -6,6 +6,6 @@ import lombok.Data;
 public class Pet {
 
 	String id,shaId, dogcat, name, gender, kind, birthday
-	, weight,weightId, g, comment, profile;
+	, weight,weightId, g, comment, profile,temId;
 	
 }
