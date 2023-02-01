11eb07596faf66610a2da5e65855dc9e86c9af9e
flown222 <jangu3384@gmail.com>
2023-02-02 00:15:45 +0900

    ⚡️ ::  멤버 메인컨트롤러 수정 -동현
    ⚡️ ::  멤버 메인컨트롤러 수정 -동현

--- src/main/java/petpple/kiwi/member/controller/Member/MemberMainController.java
+++ src/main/java/petpple/kiwi/member/controller/Member/MemberMainController.java
@@ -12,55 +12,96 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import petpple.kiwi.member.domain.member.Member;
+import petpple.kiwi.member.domain.service.Refund;
+import petpple.kiwi.member.domain.visitService.VisitService;
 import petpple.kiwi.member.repository.member.MemberMapper;
+import petpple.kiwi.member.repository.service.IReservedService;
 
 
 @Controller
-public class MemberMainController
-{
+public class MemberMainController {
 
-	@Autowired
-	private SqlSession sqlSession;
+    @Autowired
+    private SqlSession sqlSession;
 
-//	-------------- 의뢰인 메인페이지  ------------------
-	// 중간 메인 화면
-	@RequestMapping(value = "/member/memberMain")
+    //	-------------- 의뢰인 메인페이지  ------------------
+    // 중간 메인 화면
+    @RequestMapping(value = "/member/memberMain")
 //	public String memberMain(ModelMap modelMap, HttpServletRequest req, HttpServletResponse resp, HttpSession session)
-	public String memberMain(Model model, HttpServletRequest request)
-	{
-		MemberMapper dao = sqlSession.getMapper(MemberMapper.class);
+    public String memberMain(Model model, HttpServletRequest request) {
+        MemberMapper dao = sqlSession.getMapper(MemberMapper.class);
 
-		HttpSession session = request.getSession();
-	      
-		String temId = (String)session.getAttribute("temId");
-		
-		// 현재 진행중인 펫시팅
-		ArrayList<Member> currentPetsitting = dao.currentPetsitting(temId);
-		
-		// 서비스 이용 확정 대기
-		ArrayList<Member> comfirmWaiting = dao.comfirmWaiting(temId);
-		
-		// 서비스 진행 예정 펫시팅
-		ArrayList<Member> petsittingSchedule = dao.petsittingSchedule(temId);
-		
-		model.addAttribute("currentPetsitting", currentPetsitting);
+        HttpSession session = request.getSession();
 
-		model.addAttribute("comfirmWaiting", comfirmWaiting);
-		
-		model.addAttribute("petsittingSchedule", petsittingSchedule);
+        String temId = (String) session.getAttribute("temId");
+
+        // 현재 진행중인 펫시팅
+        ArrayList<Member> currentPetsitting = dao.currentPetsitting(temId);
+
+        // 서비스 이용 확정 대기
+        ArrayList<Member> comfirmWaiting = dao.comfirmWaiting(temId);
+
+        // 서비스 진행 예정 펫시팅
+        ArrayList<Member> petsittingSchedule = dao.petsittingSchedule(temId);
+
+        model.addAttribute("currentPetsitting", currentPetsitting);
+
+        model.addAttribute("comfirmWaiting", comfirmWaiting);
+
+        model.addAttribute("petsittingSchedule", petsittingSchedule);
 
 //		System.out.println(memberIng);
 //		System.out.println(dao.currentPetsitting());
 
-		return "member/memberMain";
-	}
-	// 오른쪽 달력(일정) 화면 사이드
-	@RequestMapping(value = "/member/memberCal")
-	public String memberCal()
-	{
-		return "member/memberCal";
-	}
+        return "member/memberMain";
+    }
+
+    @RequestMapping(value = "/member/serviceCancel")
+    public String serviceCancel(@RequestParam("serviceType") int serviceType, @RequestParam("serviceNo") String serviceNo) {
+        IReservedService dao = sqlSession.getMapper(IReservedService.class);
+        if (serviceType == 1) { //방문서비스 일 경우
+            Refund dto = dao.getVServiceInfo(serviceNo);
+            dao.visitServiceCancel(dto);
+
+        } else if (serviceType == 2) {
+            Refund dto = dao.getFServiceInfo(serviceNo);
+            dao.fosterServiceCancel(dto);
+        } else if (serviceType == 3) {
+            Refund dto = dao.getUVServiceInfo(serviceNo);
+            dao.urgentVServiceCancel(dto);
+        } else if (serviceType == 4) {
+            Refund dto = dao.getUFServiceInfo(serviceNo);
+            dao.urgentFServiceCancel(dto);
+        }
+        return "redirect:/member/memberMain";
+    }
+
+    @RequestMapping(value = "/member/serviceNoshow")
+    public String serviceNoshow(@RequestParam("serviceType") int serviceType, @RequestParam("serviceNo") String serviceNo) {
+        IReservedService dao = sqlSession.getMapper(IReservedService.class);
+        if (serviceType == 1) { //방문서비스 일 경우
+            Refund dto = dao.getVServiceInfo(serviceNo);
+            dao.visitServiceNoShow(dto);
+        } else if (serviceType == 2) {
+            Refund dto = dao.getFServiceInfo(serviceNo);
+            dao.fosterServiceNoShow(dto);
+        } else if (serviceType == 3) {
+            Refund dto = dao.getUVServiceInfo(serviceNo);
+            dao.uvServiceNoShow(dto);
+        } else if (serviceType == 4) {
+            Refund dto = dao.getUFServiceInfo(serviceNo);
+            dao.ufServiceNoShow(dto);
+        }
+        return "redirect:/member/memberMain";
+    }
+
+    // 오른쪽 달력(일정) 화면 사이드
+    @RequestMapping(value = "/member/memberCal")
+    public String memberCal() {
+        return "member/memberCal";
+    }
 	/*
 	// 왼쪽 펫시팅 정보 사이드
 	 
@@ -71,7 +112,7 @@ public class MemberMainController
 	}
 	*/
 
-	// 왼쪽 펫시팅 정보 사이드
+    // 왼쪽 펫시팅 정보 사이드
 	/*
 	@RequestMapping(value = "/member/memberSide")
 	public int memberSide(Model model)
@@ -87,29 +128,28 @@ public class MemberMainController
 	}
 	*/
 
-	@RequestMapping(value = "/member/memberSide", method = RequestMethod.GET)
-	public String memberSide(Model model, HttpServletRequest request)
-	{
-		// IMemberMapper 수정 필요
-		MemberMapper dao = sqlSession.getMapper(MemberMapper.class);
+    @RequestMapping(value = "/member/memberSide", method = RequestMethod.GET)
+    public String memberSide(Model model, HttpServletRequest request) {
+        // IMemberMapper 수정 필요
+        MemberMapper dao = sqlSession.getMapper(MemberMapper.class);
 
 //		model.addAttribute("count", dao.getCount());
-		
-		HttpSession session = request.getSession();
-	      
-		String temId = (String)session.getAttribute("temId");
+
+        HttpSession session = request.getSession();
+
+        String temId = (String) session.getAttribute("temId");
 
 
-		model.addAttribute("result1",dao.waitingAcceptance(temId));
+        model.addAttribute("result1", dao.waitingAcceptance(temId));
 
-		model.addAttribute("result2",dao.sumPetsitting());
+        model.addAttribute("result2", dao.sumPetsitting());
 
-		model.addAttribute("result3",dao.waitingFacceptance());
+        model.addAttribute("result3", dao.waitingFacceptance());
 
-		model.addAttribute("result4",dao.sumFpetsitting());
+        model.addAttribute("result4", dao.sumFpetsitting());
 
 
-		return "member/memberSide";
-	}
+        return "member/memberSide";
+    }
 
 }
