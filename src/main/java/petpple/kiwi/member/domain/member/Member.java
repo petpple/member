d7fb827f9c40b0c0a067282fa4d2adba169e258a
flown222 <jangu3384@gmail.com>
2023-02-02 00:15:59 +0900

    ⚡️ ::  멤버dto 수정-동현
    ⚡️ ::  멤버dto 수정-동현

--- src/main/java/petpple/kiwi/member/domain/member/Member.java
+++ src/main/java/petpple/kiwi/member/domain/member/Member.java
@@ -7,6 +7,6 @@ public class Member {
 //요청번호
 //   	private int vId;
 // 방문 요청번호, 이름, 주소, 서비스시작일, 종료일, 펫시터 닉네임, 펫시터 프로필
-private String id, temId, vid, name, addr, startDate, endDate, petsitterNickname
-, petsitterProfile, reqDate, totalPrice, type, vresId ;
+private String id, temId, serviceId, name, addr, startDate, endDate, petsitterNickname
+, petsitterProfile, reqDate, totalPrice, type, resId ,serviceType ;
 }
