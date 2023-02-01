39a5619a16808c1bcd6510131d9ea86282c97ca0
flown222 <jangu3384@gmail.com>
2023-02-02 00:16:49 +0900

    ⚡️ :: 이미지 업로드 클래스 수정(다중 업로드 메소드)-동현
    ⚡️ :: 이미지 업로드 클래스 수정(다중 업로드 메소드)-동현

--- src/main/java/petpple/kiwi/member/service/member/ImgUpload.java
+++ src/main/java/petpple/kiwi/member/service/member/ImgUpload.java
@@ -5,51 +5,46 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class ImgUpload {
-	
-	private final String PetProfilePath = "C:\\Petpple\\member\\src\\main\\resources\\static\\images\\member\\pet";
 
-	public String getPetProfilePath() {
-		return PetProfilePath;
-	}
+    private final String PetProfilePath = "C:\\Petpple\\member\\src\\main\\resources\\static\\images\\member\\pet";
 
-	// 단일 파일 업로드 저장이름 : id +확장자.
-	public String uploadProfileImg(MultipartFile uploadFile,String path ,String path2, String name) {
-		String origin = uploadFile.getOriginalFilename();
-		String ext = origin.substring(origin.lastIndexOf("."));
-		File savefile = new File(path,name+ext);
-		Path source = Paths.get(path+name+ext);
-		Path target = Paths.get(path2+name+ext);
-		try {
-			uploadFile.transferTo(savefile);
-			
-			Files.copy(source,target, StandardCopyOption.REPLACE_EXISTING);
-		} catch (Exception e) {
-			System.out.println(e.toString());
-		}
-		return name+ext; //db에 저장될 사진 파일명
-	}
-	
-	// 복수 파일 업로드
-	public void uploadImages(MultipartFile[] uploadFiles) {
-		// 업로드 경로
-		int num = 0;
+    public String getPetProfilePath() {
+        return PetProfilePath;
+    }
 
-		for (MultipartFile multipartFile : uploadFiles) {
+    // 단일 파일 업로드 저장이름 : id +확장자.
+    public String uploadProfileImg(MultipartFile uploadFile, String path, String path2, String name) {
+        String origin = uploadFile.getOriginalFilename();
+        String ext = origin.substring(origin.lastIndexOf("."));
+        File savefile = new File(path, name + ext);
+        Path source = Paths.get(path + name + ext);
+        Path target = Paths.get(path2 + name + ext);
+        try {
+            uploadFile.transferTo(savefile);
 
-			File savefile = new File(PetProfilePath, String.valueOf(++num) + ".png");
+            Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        return name + ext; //db에 저장될 사진 파일명
+    }
 
-			try {
-				multipartFile.transferTo(savefile);
-			} catch (Exception e) {
-				System.out.println(e.toString());
-			}
-		}
-
-	}
+    // 복수 파일 업로드
+    public List<String>  uploadImages(MultipartFile[] uploadFiles, String sitterPath, String memberPath, String name) {
+        // 업로드 경로
+        int num = 0;
+        List<String> fileNames = new ArrayList<>();
+        for (MultipartFile multipartFile : uploadFiles) {
+          fileNames.add(uploadProfileImg(multipartFile, sitterPath, memberPath, name + (num++)));
+        }
+        return fileNames;
+    }
 }
