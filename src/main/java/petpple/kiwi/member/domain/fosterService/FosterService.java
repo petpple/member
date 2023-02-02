9875d67af44a21576a405773faa8349410b7b9db
SOYEON <tivm8012@naver.com>
2023-02-02 21:29:28 +0900

    📑 :: FosterService 파일 생성 - 소연
    📑 :: FosterService 파일 생성 - 소연

---
+++ src/main/java/petpple/kiwi/member/domain/fosterService/FosterService.java
@@ -0,0 +1,12 @@
+package petpple.kiwi.member.domain.fosterService;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FosterService
+{
+	String startDate,endDate,checkIn,checkOut,careType,content,code,addr,detailAddress
+		,lat,lng,petShaId,pay,refBankId,refDepositor,refAccNum,start,end,allowId;
+}
