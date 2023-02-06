c0d230f5942b7783e269cbc7dd74bf7b3e83d13b
SOYEON <tivm8012@naver.com>
2023-02-06 21:33:07 +0900

    ⚡️ ::  MemberFosterServiceController 수정 - 소연
    ⚡️ ::  MemberFosterServiceController 수정 - 소연

--- src/main/java/petpple/kiwi/member/controller/Member/MemberFosterServiceController.java
+++ src/main/java/petpple/kiwi/member/controller/Member/MemberFosterServiceController.java
@@ -11,8 +11,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import petpple.kiwi.member.domain.apply.ReviewProfile;
 import petpple.kiwi.member.domain.apply.Sitter;
+import petpple.kiwi.member.domain.fosterService.FosterService;
+import petpple.kiwi.member.domain.pet.Pet;
 import petpple.kiwi.member.repository.fService.IFosterService;
 import petpple.kiwi.member.service.sitter.GradeCaculator;
 import petpple.kiwi.member.service.sitter.SitterRequestHelper;
@@ -54,5 +59,77 @@ public class MemberFosterServiceController
 
 		return "/member/fsitterPetSitterList";
 	}
+
+	@RequestMapping(value = "/member/fsitter")
+	public String membervSitterProfile(@RequestParam("allowid") String allowId, @RequestParam("grade") String grade,
+			Model model, HttpServletRequest request)
+	{
+		IFosterService dao = sqlSession.getMapper(IFosterService.class);
+		Sitter sitter = dao.getFSitterProfile(allowId);
+		sitter.setGrade(grade);
+		model.addAttribute("sitter", sitter);
+
+		ArrayList<ReviewProfile> reviews = dao.getReviews(allowId);
+		model.addAttribute("reviews", reviews);
+
+		HttpSession session = request.getSession();
+
+		String temId = (String) session.getAttribute("temId");
+
+		if (temId == null) {
+			return "//user/userMain";
+		}
+
+		ArrayList<Pet> petList = dao.getPet(temId);
+		model.addAttribute("petList", petList);
+
+		return "/member/fsitterMyProfile";
+	}
+
+	@RequestMapping(value = "/member/showMoreFList", method = RequestMethod.POST)
+	public String membershowMoreVList(HttpServletRequest request, @RequestParam("start") int start,
+			@RequestParam("end") int end, Model model) {
+
+		IFosterService dao = sqlSession.getMapper(IFosterService.class);
+
+		HttpSession session = request.getSession();
+		String temId = (String)session.getAttribute("temId");
+		if(temId==null)
+		{
+			return "//user/userMain";
+		}
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("start", start);
+		map.put("end", end);
+		map.put("temId",temId);
+		ArrayList<Sitter> list = dao.getFSitterList(map);
+		for (Sitter sitter : list) {
+			sitter.setGrade(gc.getGrade(sitter.getTime(), sitter.getCount()));
+		}
+
+		model.addAttribute("list", list);
+		return "/member/fsitterAjaxList";
+	}
+
+	@RequestMapping(value = "/member/fosterServiceRequest", method = RequestMethod.POST)
+	public String memberFosterServiceRequest(FosterService vs, @RequestParam("petShaIdArr") String[] petShaIdArr)
+	{
+		IFosterService dao = sqlSession.getMapper(IFosterService.class);
+		vs.setStart(srh.getDate(vs.getStartDate(), vs.getCheckIn()));
+		vs.setEnd(srh.getDate(vs.getEndDate(), vs.getCheckOut()));
+		dao.requestFosterService(vs);
+		for (String id : petShaIdArr) {
+			dao.requestPetChoice(id);
+		}
+
+		return "/member/success";
+	}
+//
+//	@RequestMapping(value = "/member/success")
+//	public String memberRequestSuccess() {
+//
+//
+//		return "/member/success";
+//	}
 
 }
