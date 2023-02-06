af280cd619aa34d9708110d6b9be5cd0f8f7e218
SOYEON <tivm8012@naver.com>
2023-02-06 21:30:39 +0900

    ⚡️ ::  Sitter DTO  속성 수정 - 소연
    ⚡️ ::  Sitter DTO  속성 수정 - 소연

--- src/main/java/petpple/kiwi/member/domain/apply/Sitter.java
+++ src/main/java/petpple/kiwi/member/domain/apply/Sitter.java
@@ -4,11 +4,11 @@ import lombok.Data;
 
 @Data
 public class Sitter {
-	String id,allowId,addr,lat,lng  //id는 지원 번호 r
-	,title,name, content, depositor
-	, accNumber , reviewCnt ,grade
-	, smokeId, petSizeId,profileImg
-	, petKindId, bankId,time,count
-	, temId;
+	String id, allowId, addr, lat, lng  //id는 지원 번호
+	, title, name, content, depositor
+	, accNumber , reviewCnt , grade
+	, smokeId, petSizeId, profileImg
+	, petKindId, bankId, time, count
+	, temId, fImg1, fImg2, fImg3;
 	int stars;
 }
